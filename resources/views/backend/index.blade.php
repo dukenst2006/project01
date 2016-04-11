3df4c5bca633b594ad3c3a66e37087f7383bf7d6
Thelemaque <dukenst2006@gmail.com>
2016-04-10 20:57:08 -0400

    update sidebar
    update sidebar

--- resources/views/backend/index.blade.php
+++ resources/views/backend/index.blade.php
@@ -1,21 +1,21 @@
 @extends ('backend.layouts.master')
 
-@section ('title', trans('labels.backend.access.users.management'))
+@section ('title', trans('labels.backend.access.customers.management'))
 
 @section('page-header')
     <h1>
-        {{ trans('labels.backend.access.users.management') }}
-        <small>{{ trans('labels.backend.access.users.active') }}</small>
+        {{ trans('labels.backend.access.customers.management') }}
+        <small>{{ trans('labels.backend.access.customers.active') }}</small>
     </h1>
 @endsection
 
 @section('content')
     <div class="box box-success">
         <div class="box-header with-border">
-            <h3 class="box-title">{{ trans('labels.backend.access.users.active') }}</h3>
+            <h3 class="box-title">{{ trans('labels.backend.access.customers.active') }}</h3>
 
             <div class="box-tools pull-right">
-                @include('backend.access.includes.partials.header-buttons')
+                @include('backend.access.includes.partials.customer-header-buttons')
             </div>
         </div><!-- /.box-header -->
 
@@ -24,45 +24,29 @@
                 <table class="table table-striped table-bordered table-hover">
                     <thead>
                     <tr>
-                        <th>{{ trans('labels.backend.access.users.table.id') }}</th>
-                        <th>{{ trans('labels.backend.access.users.table.name') }}</th>
-                        <th>{{ trans('labels.backend.access.users.table.email') }}</th>
-                        <th>{{ trans('labels.backend.access.users.table.confirmed') }}</th>
-                        <th>{{ trans('labels.backend.access.users.table.roles') }}</th>
-                        <th>{{ trans('labels.backend.access.users.table.other_permissions') }}</th>
-                        <th class="visible-lg">{{ trans('labels.backend.access.users.table.created') }}</th>
-                        <th class="visible-lg">{{ trans('labels.backend.access.users.table.last_updated') }}</th>
+                        <th>{{ trans('labels.backend.access.customers.table.id') }}</th>
+                        <th>{{ trans('labels.backend.access.customers.table.name') }}</th>
+                        <th>{{ trans('labels.backend.access.customers.table.email') }}</th>
+                        <th>{{ trans('labels.backend.access.customers.table.confirmed') }}</th>
+                        <th>{{ trans('labels.backend.access.customers.table.roles') }}</th>
+                        <th>{{ trans('labels.backend.access.customers.table.other_permissions') }}</th>
+                        <th class="visible-lg">{{ trans('labels.backend.access.customers.table.created') }}</th>
+                        <th class="visible-lg">{{ trans('labels.backend.access.customers.table.last_updated') }}</th>
                         <th>{{ trans('labels.general.actions') }}</th>
                     </tr>
                     </thead>
                     <tbody>
-                    @foreach ($users as $user)
+                    @foreach ($customers as $customer)
                         <tr>
-                            <td>{!! $user->id !!}</td>
-                            <td>{!! $user->name !!}</td>
-                            <td>{!! link_to("mailto:".$user->email, $user->email) !!}</td>
-                            <td>{!! $user->confirmed_label !!}</td>
-                            <td>
-                                @if ($user->roles()->count() > 0)
-                                    @foreach ($user->roles as $role)
-                                        {!! $role->name !!}<br/>
-                                    @endforeach
-                                @else
-                                    {{ trans('labels.general.none') }}
-                                @endif
-                            </td>
-                            <td>
-                                @if ($user->permissions()->count() > 0)
-                                    @foreach ($user->permissions as $perm)
-                                        {!! $perm->display_name !!}<br/>
-                                    @endforeach
-                                @else
-                                    {{ trans('labels.general.none') }}
-                                @endif
-                            </td>
-                            <td class="visible-lg">{!! $user->created_at->diffForHumans() !!}</td>
-                            <td class="visible-lg">{!! $user->updated_at->diffForHumans() !!}</td>
-                            <td>{!! $user->action_buttons !!}</td>
+                            <td>{!! $customer->id !!}</td>
+                            <td>{!! $customer->name.' '.$customer->lastname !!}</td>
+                            <td>{!! link_to("mailto:".$customer->email, $customer->email) !!}</td>
+                            <td>{!! $customer->occupation !!}</td>
+                            <td>{!! $customer->phone !!}</td>
+                            <td>{!! $customer->address !!}</td>
+                            <td class="visible-lg">{!! $customer->created_at->diffForHumans() !!}</td>
+                            <td class="visible-lg">{!! $customer->updated_at->diffForHumans() !!}</td>
+                            <td>{!! $customer->action_buttons !!}</td>
                         </tr>
                     @endforeach
                     </tbody>
@@ -70,11 +54,11 @@
             </div>
 
             <div class="pull-left">
-                {!! $users->total() !!} {{ trans_choice('labels.backend.access.users.table.total', $users->total()) }}
+                {!! App\Customer::count() !!} {{ trans_choice('labels.backend.access.customers.table.total', App\Customer::count()) }}
             </div>
 
             <div class="pull-right">
-                {!! $users->render() !!}
+
             </div>
 
             <div class="clearfix"></div>
